230c7fb625c1384b0e759bf2f518bbb58a90da29
kiang <vancur2021@gmail.com>
2025-08-29 19:26:10 +0800

    优化页面+1

--- src/components/column/card.tsx
+++ src/components/column/card.tsx
@@ -33,7 +33,7 @@ export const CardWrapper = forwardRef<HTMLElement, ItemsProps>(({ id, isDragging
     <div
       ref={ref}
       className={$(
-        "flex flex-col h-500px rounded-2xl p-4 cursor-default",
+        "flex flex-col h-[calc(100vh-120px)] rounded-2xl p-4 cursor-default",
         // "backdrop-blur-5",
         "transition-opacity-300",
         isDragging && "op-50",
